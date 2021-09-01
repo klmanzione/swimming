1ca64baaea6d65dbb4fee1af52ff5926e13e9597
Ryan Elmore <Ryan.Elmore@du.edu>
2021-09-01 10:25:35 -0600

    positive improvement

--- analysis.R
+++ analysis.R
@@ -12,13 +12,15 @@ dplyr::group_by(df, event, School) %>%
   dplyr::summarize(n = n())
 
 df <- df %>% 
-  dplyr::mutate(., event_long = paste(event, "yard freestyle", sep = " "),
+  dplyr::mutate(., 
+                pos_improvement = -improvement,
+                event_long = paste(event, "yard freestyle", sep = " "),
                 event_reorder = factor(event_long, levels = c('50 yard freestyle',
                                                               '200 yard freestyle',
                                                               '1650 yard freestyle')))
 
 p <- ggplot(data = df,
-            aes(x = elevation_difference, y = improvement, 
+            aes(x = elevation_difference, y = pos_improvement, 
                 group = event_reorder, col = event_reorder, pch = event_reorder))
 p + geom_point(alpha = .5) +
   geom_smooth(method = "lm", alpha = .5) +
@@ -33,28 +35,34 @@ ggsave("fig/regression-fits.png", hei = 6, wid = 8)
 ## Figs in paper
 
 p <- ggplot(data = df,
-            aes(x = improvement, fill = Altitude))
+            aes(x = pos_improvement, fill = Altitude))
 p + geom_density(alpha = .5) +
   facet_wrap(~ factor(event_long, levels = c('50 yard freestyle',
                                              '200 yard freestyle',
                                              '1650 yard freestyle')), 
              nrow = 3, scales = "free") +
   scale_fill_brewer(palette = "Dark2") +
+  labs(x = "improvement") +
   theme_bw()
 
+ggsave("density-plots.png", wid = 7, hei = 9)
+
 p <- ggplot(data = df,
-            aes(x = School, y = improvement, fill = Altitude))
+            aes(x = School, y = pos_improvement, fill = Altitude))
 p + geom_boxplot() +
   facet_wrap(~ factor(event_long, levels = c('50 yard freestyle',
                                              '200 yard freestyle',
                                              '1650 yard freestyle')), 
              nrow = 3, scales = "free") +
   scale_fill_brewer(palette = "Dark2") +
+  labs(y = "improvment") +
   theme_bw()
+
+ggsave("boxplots-plots.png", wid = 7, hei = 8)
 
 ## Regression models
 
-swim_lm <- lm(improvement ~ elevation_difference*event,
+swim_lm <- lm(pos_improvement ~ elevation_difference*event,
               data = df)
 summary(swim_lm)
 
